2626fe3847ace601913af83181ee715254b4e83e
Bickson Ningombam <bickson@globizs.com>
2025-06-17 05:10:49 +0000

    make the 4 journal in grid 2 row like in image I provided

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,7 @@ export default function HomePage() {
               </p>
             </div>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 md:gap-8">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6 md:gap-8">
               {journalCategories.map((category) => (
                 <JournalPublicationCard 
                   key={category.id} 
