4d419663469f5c720460e52309d6e43e80663145
Bickson Ningombam <bickson@globizs.com>
2025-06-21 05:12:26 +0000

    in submit manuscript upload files step react is not defined

--- src/components/author/forms/AuthorDetailsForm.tsx
+++ src/components/author/forms/AuthorDetailsForm.tsx
@@ -1,6 +1,7 @@
 
 'use client';
 
+import React from 'react';
 import { useForm, useFieldArray, Controller } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
